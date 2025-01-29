be9352305f16e31b4bc8e2a6df2e497d11846951
Vickram Abdul Faqih <86212161+Vickram-Priyono@users.noreply.github.com>
2025-01-30 01:33:28 +0700

    add nav bar

--- Tabs/app.js
+++ Tabs/app.js
@@ -6,16 +6,13 @@ for (let button of btn) {
     tabs[0].style.display = "none";
     tabs[1].style.display = "none";
     tabs[2].style.display = "none";
-    tabs[3].style.display = "none";
 
     if (button.id === "btn-1") {
       tabs[0].style.display = "block";
     } else if (button.id === "btn-2") {
       tabs[1].style.display = "block";
-    } else if (button.id === "btn-3") {
-      tabs[2].style.display = "block";
     } else {
-      tabs[3].style.display = "block";
+      tabs[2].style.display = "block";
     }
   });
 }
