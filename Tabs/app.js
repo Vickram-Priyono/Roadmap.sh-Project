0b0c29a1674c982ef092e45f0bb51d50e2d12526
Vickram Abdul Faqih <86212161+Vickram-Priyono@users.noreply.github.com>
2025-01-30 00:29:58 +0700

    style menu page

--- Tabs/app.js
+++ Tabs/app.js
@@ -1,21 +1,21 @@
 const btn = document.querySelectorAll(".btn-tab");
-const tab = document.querySelectorAll(".tab");
+const tabs = document.querySelectorAll(".tab");
 
 for (let button of btn) {
   button.addEventListener("click", () => {
-    tab[0].style.display = "none";
-    tab[1].style.display = "none";
-    tab[2].style.display = "none";
-    tab[3].style.display = "none";
+    tabs[0].style.display = "none";
+    tabs[1].style.display = "none";
+    tabs[2].style.display = "none";
+    tabs[3].style.display = "none";
 
     if (button.id === "btn-1") {
-      tab[0].style.display = "block";
+      tabs[0].style.display = "block";
     } else if (button.id === "btn-2") {
-      tab[1].style.display = "block";
+      tabs[1].style.display = "block";
     } else if (button.id === "btn-3") {
-      tab[2].style.display = "block";
+      tabs[2].style.display = "block";
     } else {
-      tab[3].style.display = "block";
+      tabs[3].style.display = "block";
     }
   });
 }
