661958f7dcc0c3725ecf92b91988433ccc6b0948
Vickram Abdul Faqih <86212161+Vickram-Priyono@users.noreply.github.com>
2025-01-30 04:51:24 +0700

    map

--- app.js
+++ app.js
@@ -16,3 +16,11 @@ for (let button of btn) {
     }
   });
 }
+
+var map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
