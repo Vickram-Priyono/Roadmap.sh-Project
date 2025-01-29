c287db2e06df0db409327c54efda8f6599b8aa59
Vickram Abdul Faqih <86212161+Vickram-Priyono@users.noreply.github.com>
2025-01-30 05:10:58 +0700

    tambahkan map

--- app.js
+++ app.js
@@ -1,26 +1,61 @@
-const btn = document.querySelectorAll(".btn-tab");
+// Tab Handler
 const tabs = document.querySelectorAll(".tab");
+const btns = document.querySelectorAll(".btn-tab");
+let map = null; // Variabel untuk menyimpan instance map
+let isMapInitialized = false; // Flag untuk cek inisialisasi
 
-for (let button of btn) {
-  button.addEventListener("click", () => {
-    tabs[0].style.display = "none";
-    tabs[1].style.display = "none";
-    tabs[2].style.display = "none";
+function showTab(tabId) {
+  tabs.forEach((tab) => {
+    if (tab.id === tabId) {
+      tab.classList.remove("hide");
 
-    if (button.id === "btn-1") {
-      tabs[0].style.display = "block";
-    } else if (button.id === "btn-2") {
-      tabs[1].style.display = "block";
+      // Inisialisasi map hanya saat tab lokasi ditampilkan pertama kali
+      if (tabId === "tab-3" && !isMapInitialized) {
+        initializeMap();
+        isMapInitialized = true;
+      }
+
+      // Paksa update ukuran map setelah tab ditampilkan
+      if (tabId === "tab-3" && map) {
+        setTimeout(() => {
+          map.invalidateSize();
+        }, 50);
+      }
     } else {
-      tabs[2].style.display = "block";
+      tab.classList.add("hide");
     }
   });
 }
 
-var map = L.map("map").setView([51.505, -0.09], 13);
+function initializeMap() {
+  // Buat instance map
+  map = L.map("map").setView([-6.157979, 106.845844], 16);
 
-L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 19,
-  attribution:
-    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-}).addTo(map);
+  // Tambahkan tile layer
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  // Tambahkan marker
+  L.marker([-6.157979, 106.845844])
+    .addTo(map)
+    .bindPopup("Ayam Appolo")
+    .openPopup();
+
+  // Handle window resize
+  window.addEventListener("resize", () => {
+    map.invalidateSize();
+  });
+}
+
+// Event listener untuk tombol tab
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const tabId = e.target.id.replace("btn", "tab");
+    showTab(tabId);
+  });
+});
+
+// Buka tab pertama secara default
+showTab("tab-1");
